36c0ff6ab6e0161399f1f5861ef1bb159ebc1129
Alexandre Mariano <alexandre_mariano@hotmail.com.br>
2023-08-04 17:18:14 -0300

    chore: apagando pastas anteriores e deixando o repositório independente

---
+++ test.sh
@@ -0,0 +1,4 @@
+curl --request POST \
+    --url http://localhost:3333/signup \
+    --header 'Content-Type: application/json' \
+    --data '{"email": "caroline@example.com", "password": "pwd123"}'
