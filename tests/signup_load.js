7c6c5ff16156bdd909c59d9e988d329d224a149d
Alexandre <alexandre.lightbase@sebraemg.com.br>
2023-08-08 16:53:49 -0300

    final: final commit

--- tests/signup_load.js
+++ tests/signup_load.js
@@ -1,11 +1,14 @@
 import http from 'k6/http'
 import { sleep, check } from 'k6'
 import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
-
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
-    vus: 10,
-    duration: '5s',
+    stages: [
+        {duration: '1m', target: 100},
+        {duration: '2m', target: 100},
+        {duration: '1m', target: 0}
+    ],
     thresholds: {
         http_req_duration: ['p(95)<2000'], // 95% das requisições devem responder em até 2 segundos
         http_req_failed: ['rate<0.01'] // 1% das requisiçoes podem ocorrer erro
@@ -37,3 +40,9 @@ export default function () {
 
     sleep(1)
 }
+
+export function handleSummary(data) {
+    return {
+        "summary.html": htmlReport(data),
+    };
+    }
--- tests/signup_smoke.js
+++ tests/signup_smoke.js
@@ -1,11 +1,11 @@
 import http from 'k6/http'
 import { sleep, check } from 'k6'
 import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
-
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
-    vus: 10,
-    duration: '5s',
+    vus: 1,
+    duration: '15s',
     thresholds: {
         http_req_duration: ['p(95)<2000'], // 95% das requisições devem responder em até 2 segundos
         http_req_failed: ['rate<0.01'] // 1% das requisiçoes podem ocorrer erro
@@ -37,3 +37,9 @@ export default function () {
 
     sleep(1)
 }
+
+export function handleSummary(data) {
+    return {
+        "summary.html": htmlReport(data),
+    };
+    }
